1cad89e0349ae56188dd2d877121dcd61820c4f3
Jianyi <jianyishi1995@outlook.com>
2018-04-10 17:07:28 -0400

    add another version

--- binary-search/MinimumInRotatedArray.java
+++ binary-search/MinimumInRotatedArray.java
@@ -1,6 +1,7 @@
 public class MinimumInRotatedArray {
 
-	public static findMin(int[] arr) {
+	// Version1
+	public int findMin(int[] arr) {
 		if (arr == null || arr.length == 0)
 			return 0;
 		int left = 0;
@@ -14,5 +15,25 @@ public class MinimumInRotatedArray {
 				right = mid;
 		}
 		return Math.min(nums[left], nums[right]);
+	}
+
+	// Version2
+	public int findMin(int[] arr) {
+		if (arr == null || arr.length == 0)
+			return 0;
+		int left = 0;
+		int right = arr.length - 1;
+
+		while (left < right) {
+			int mid = (left + right) / 2;
+			if (mid > 0 && arr[mid] < arr[mid - 1])
+				return arr[mid];
+
+			if (arr[mid] >= arr[left] && arr[mid] > arr[right])
+				left = mid + 1;
+			else
+				right = mid - 1;
+		}
+		return arr[left];
 	}
 }
