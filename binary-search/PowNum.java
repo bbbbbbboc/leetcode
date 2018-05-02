2dfb31b5a8fba963775fc74f4d9d047fbf06483f
Jianyi <jianyishi1995@outlook.com>
2018-05-01 23:11:58 -0400

    50. pow number

--- binary-search/PowNum.java
+++ binary-search/PowNum.java
@@ -1,20 +1,15 @@
 class Solution {
     public double myPow(double x, int n) {
         
-        if (n == 0)
-            return 1;
-        
+        if (n < 0)
+            return 1 / myPow(x, -n);
+
         double half = myPow(x, n / 2);
-        
+
         if (n % 2 == 0) {
             return half * half;
         } else {
-            if (n < 0) {
-                return 1 / x * half * half;
-            } else {
-                return x * half * half;
-            }
+            return x * half * half;
         }
-            
     }
 }
