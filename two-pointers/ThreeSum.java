e5ed384486b29cf7955420a6b0f3d1bc9b21ad36
Jianyi <jianyishi1995@outlook.com>
2018-05-06 16:14:19 -0400

    add v2 3sum

--- two-pointers/ThreeSum.java
+++ two-pointers/ThreeSum.java
@@ -39,4 +39,43 @@ class Solution {
             }
         }
     }
+
+    public List<List<Integer>> threeSumV2(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums == null || nums.length == 0)
+            return ans;
+        
+        Arrays.sort(nums);
+        
+        for (int i = 0; i <= nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] == 0) {
+                    List<Integer> curr = new ArrayList<>();
+                    curr.add(nums[i]);
+                    curr.add(nums[left]);
+                    curr.add(nums[right]);
+                    ans.add(curr);
+                    
+                    left++;
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    right--;
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                    
+                } else if (nums[i] + nums[left] + nums[right] < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        
+        return ans;
+    }
 }
