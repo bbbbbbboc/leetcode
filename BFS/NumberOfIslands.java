dbe5609d4a245720d37ce4c44c9f296117c4fe40
Jianyi <jianyishi1995@outlook.com>
2018-05-09 18:39:14 -0400

    add unionfind numberOfIslands

--- BFS/NumberOfIslands.java
+++ BFS/NumberOfIslands.java
@@ -52,87 +52,78 @@ class Solution {
 
 
 
-
-
-// version 2: Union Find
-class UnionFind { 
-
-    private int[] father = null;
-    private int count;
-
-    private int find(int x) {
-        if (father[x] == x) {
-            return x;
-        }
-        return father[x] = find(father[x]);
-    }
-
-    public UnionFind(int n) {
-        // initialize your data structure here.
-        father = new int[n];
-        for (int i = 0; i < n; ++i) {
-            father[i] = i;
-        }
-    }
-
-    public void connect(int a, int b) {
-        // Write your code here
-        int root_a = find(a);
-        int root_b = find(b);
-        if (root_a != root_b) {
-            father[root_a] = root_b;
-            count --;
-        }
-    }
+class Solution {
+    int[] dx = new int[]{0, 1, 0, -1};
+    int[] dy = new int[]{1, 0, -1, 0};
+    
+    class UnionFind {
+        int count;
+        int[] father;
         
-    public int query() {
-        // Write your code here
-        return count;
+        UnionFind(int n) {
+            father = new int[n];
+            for (int i = 0; i < n; i++) {
+                father[i] = i;
+            }
+        }
+        
+        void connect(int a, int b) {
+            int rootA = findRoot(a);
+            int rootB = findRoot(b);
+            if (rootA != rootB) {
+                father[rootA] = rootB;
+                count--;
+            }
+        }
+        
+        int findRoot(int n) {
+            if (father[n] == n)
+                return n;
+            return father[n] = findRoot(father[n]);
+        }
+        
+        void setCount(int count) {
+            this.count = count;
+        }
+        
+        int query() {
+            return count;
+        }
     }
     
-    public void set_count(int total) {
-        count = total;
-    }
-}
-
-public class Solution {
-    /**
-     * @param grid a boolean 2D matrix
-     * @return an integer
-     */
-    public int numIslands(boolean[][] grid) {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0)
+            return 0;
+        
         int count = 0;
         int n = grid.length;
-        if (n == 0)
-            return 0;
         int m = grid[0].length;
-        if (m == 0)
-            return 0;
-        UnionFind union_find = new UnionFind(n * m);
+        UnionFind uf = new UnionFind(n * m);
         
-        int total = 0;
-        for(int i = 0;i < grid.length; ++i)
-            for(int j = 0;j < grid[0].length; ++j)
-            if (grid[i][j])
-                total ++;
-    
-        union_find.set_count(total);
-        for(int i = 0;i < grid.length; ++i)
-            for(int j = 0;j < grid[0].length; ++j)
-            if (grid[i][j]) {
-                if (i > 0 && grid[i - 1][j]) {
-                    union_find.connect(i * m + j, (i - 1) * m + j);
-                }
-                if (i <  n - 1 && grid[i + 1][j]) {
-                    union_find.connect(i * m + j, (i + 1) * m + j);
-                }
-                if (j > 0 && grid[i][j - 1]) {
-                    union_find.connect(i * m + j, i * m + j - 1);
-                }
-                if (j < m - 1 && grid[i][j + 1]) {
-                    union_find.connect(i * m + j, i * m + j + 1);
-                }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1')
+                    count++;
             }
-        return union_find.query();
+        }
+        
+        uf.setCount(count);
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '0')
+                    continue;
+                
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] == '1')
+                        uf.connect(i * m + j, nx * m + ny);
+                }
+                
+            }
+        }
+        
+        return uf.query();
     }
 }
