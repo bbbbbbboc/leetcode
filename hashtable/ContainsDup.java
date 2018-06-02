2d7c1a0c525839ec3aa0b999f9c36e8ad5706280
jianyi <shijianyi95@yeah.net>
2018-06-01 22:55:20 -0700

    a lot of hash table, pay attention to the usage of hashcode

---
+++ hashtable/ContainsDup.java
@@ -0,0 +1,18 @@
+// 217. Contains Duplicate
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return false;
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i : nums) {
+            if (set.contains(i))
+                return true;
+            set.add(i);
+        }
+        
+        return false;
+    }
+}
