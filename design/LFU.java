805adce8934b4678a78f810213ca53ca2ae753bb
Jianyi_1001 <jianyishi1995@outlook.com>
2018-08-05 16:32:15 -0700

    come on

--- design/LFU.java
+++ design/LFU.java
@@ -139,3 +139,69 @@ class LFUCache {
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
+
+
+
+
+// approach 2
+class LFUCache {
+    Map<Integer, Integer> vals;
+    Map<Integer, Integer> counts;
+    Map<Integer, LinkedHashSet<Integer>> lists;
+    int min = -1;
+    int cap;
+
+    public LFUCache(int capacity) {
+        cap = capacity;
+        vals = new HashMap<>();
+        counts = new HashMap<>();
+        lists = new HashMap<>();
+        lists.put(1, new LinkedHashSet<>());
+    }
+    
+    public int get(int key) {
+        if (!vals.containsKey(key))
+            return -1;
+        
+        int count = counts.get(key);
+        counts.put(key, count + 1);
+        lists.get(count).remove(key);
+        if (count == min && lists.get(count).size() == 0)
+            min++;
+        if (!lists.containsKey(count + 1))
+            lists.put(count + 1, new LinkedHashSet<>());
+        lists.get(count + 1).add(key);
+        
+        return vals.get(key);
+    }
+    
+    public void put(int key, int value) {
+        if (cap == 0)
+            return;
+        
+        if (vals.containsKey(key)) {
+            vals.put(key, value);
+            get(key);
+            return;
+        }
+        
+        if (vals.size() >= cap) {
+            int evictKey = lists.get(min).iterator().next();
+            lists.get(min).remove(evictKey);
+            vals.remove(evictKey);
+            counts.remove(evictKey);
+        }
+        
+        vals.put(key, value);
+        counts.put(key, 1);
+        min = 1;
+        lists.get(1).add(key);
+    }
+}
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
